40cfde138c8fa2f2a33d83e22c1772d0b387b241
Mikyyz <youzhi.ye1992@gmail.com>
2020-12-02 20:52:06 +0800

    用户信息存储到内存中

---
+++ src/utils/memoryUtil.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 叶有志 
+ * @Date: 2019-09-21 22:46:05 
+ * @Last Modified by: 叶有志
+ * @Last Modified time: 2019-12-04 20:58:12
+ */
+
+/**
+ * 
+ * 将用户信息存储到内存中
+ */
+
+export default  {
+    user: {}
+}
