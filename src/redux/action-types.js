c5fc07f1a425e6d4bdc97ff1d81be21dc05db019
Mikyyz <youzhi.ye1992@gmail.com>
2020-12-02 20:54:34 +0800

    actionType

---
+++ src/redux/action-types.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: 叶有志 
+ * @Date: 2019-12-04 20:39:11 
+ * @Last Modified by: 叶有志
+ * @Last Modified time: 2019-12-05 01:45:06
+ */
+
+export const SET_HEAD_TITLE = 'SET_HEAD_TITLE';  //设置头部标题
+
+export const RECEIVE_USER = 'RECEIVE_USER';      //登录成功后接收用户信息
+
+export const SHOW_ERROR_MSG = 'SHOW_ERROR_MSG';  //登录失败后显示的错误提示信息
+
+export const RESET_USER = 'RESET_USER';          //用户退出登录后
+
